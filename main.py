0a5b4cfb1ff899986deff29fd5adccb537e8fff0
Calvin Figuereo-Supraner <mail@calvin.page>
2026-01-25 14:06:38 -0800

    Compute 5 top moves

--- main.py
+++ main.py
@@ -10,52 +10,73 @@ import stockfish
 
 # [{'Move': 'a2a4', 'Centipawn': 26, 'Mate': None, 'Time': 1, 'Nodes': 11031, 'MultiPVNumber': 1, 'NodesPerSecond': 11031000, 'SelectiveDepth': 9, 'PVMoves': 'a2a4 a7a5 e2e4 f7f5 e4f5 g7g6', 'WDL': '68 921 11'}]
 
-DEPTH = 7
+DEPTH = 30
+THREADS = 8
+HASH = 16384
+
 
 @dataclasses.dataclass
-class Result:
-    numbering: int
-    fen: str
-    depth: int
-    best_move: str
-    centipawns: int
-    principal_variation: list[str]
+class TopMove:
+    move: str
+    centipawns: int | None
+    mate: int | None
+    time: int
+    nodes: int
+    multipv_number: int
+    nodes_per_second: int
+    selective_depth: int
+    pv_moves: list[str]
     win: int
     draw: int
     loss: int
 
     @classmethod
-    def from_stockfish(cls, numbering: int, fen: str, depth: int, info: dict) -> "Result":
+    def from_stockfish(cls, info: dict) -> "TopMove":
         win, draw, loss = map(int, info["WDL"].split())
         return cls(
-            numbering=numbering,
-            fen=fen,
-            depth=depth,
-            best_move=info["Move"],
+            move=info["Move"],
             centipawns=info["Centipawn"],
-            principal_variation=info["PVMoves"].split(),
+            mate=info["Mate"],
+            time=info["Time"],
+            nodes=info["Nodes"],
+            multipv_number=info["MultiPVNumber"],
+            nodes_per_second=info["NodesPerSecond"],
+            selective_depth=info["SelectiveDepth"],
+            pv_moves=info["PVMoves"].split(),
             win=win,
             draw=draw,
             loss=loss,
         )
 
 
+@dataclasses.dataclass
+class Result:
+    numbering: int
+    fen: str
+    depth: int
+    top_moves: list[TopMove]
+
+
 def main() -> None:
     engine = stockfish.Stockfish(
         path="/opt/homebrew/bin/stockfish",
         depth=DEPTH,
-        parameters={"Threads": 8, "Hash": 4096, "UCI_Chess960": True},
+        parameters={"Threads": THREADS, "Hash": HASH, "UCI_Chess960": True},
     )
 
-    # FENs.txt contails the FEN list linked above:
     with open("fens.txt") as f:
         fens = f.read().splitlines()
 
     for i, fen in enumerate(fens):
         engine.set_fen_position(fen)
-        info = engine.get_top_moves(num_top_moves=1, verbose=True)
-        result = Result.from_stockfish(i, fen, DEPTH, info[0])
-        print(result)
+        info = engine.get_top_moves(num_top_moves=5, verbose=True)
+        result = Result(
+            numbering=i,
+            fen=fen,
+            depth=DEPTH,
+            top_moves=[TopMove.from_stockfish(move) for move in info],
+        )
+        print(json.dumps(dataclasses.asdict(result)))
 
 
 if __name__ == "__main__":
