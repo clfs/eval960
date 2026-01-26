f1a013d7f6a317d70557b7fe816aaa2d9170da74
Calvin Figuereo-Supraner <mail@calvin.page>
2026-01-25 22:27:58 -0800

    Improve help messages

--- main.py
+++ main.py
@@ -33,21 +33,22 @@ def main():
         type=str,
         metavar="PATH",
         required=True,
-        help="Path to the Stockfish executable.",
+        help="path to the Stockfish executable",
     )
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
         "--id",
         type=int,
+        metavar="N",
         action="append",
-        help="Specific position ID to analyze. Can be used multiple times.",
+        help="analyze position N; can be provided multiple times",
     )
     group.add_argument(
         "--range",
         type=str,
-        metavar="START-END",
-        help="Range of position IDs to analyze (e.g. 5-10).",
+        metavar="M-N",
+        help="analyze positions M through N inclusive",
     )
 
     parser.add_argument(
@@ -55,43 +56,32 @@ def main():
         type=int,
         metavar="N",
         default=None,
-        help="Set custom number of threads to use.",
+        help="set custom number of threads to use",
     )
     parser.add_argument(
         "--depth",
         type=int,
         metavar="N",
         default=20,
-        help="Set custom depth limit for analysis.",
+        help="set custom depth limit for analysis",
     )
     parser.add_argument(
         "--hash",
         type=int,
         metavar="N",
         default=None,
-        help="Set custom hash size in MB.",
+        help="set custom hash size in MB",
     )
     args = parser.parse_args()
 
     ids = set()
     if args.id:
         ids.update(args.id)
-    if args.range:
-        try:
-            start, end = map(int, args.range.split("-"))
-            if start > end:
-                raise ValueError
-            ids.update(range(start, end + 1))
-        except ValueError:
-            parser.error(f"Invalid value for --range: {args.range}")
-
-    if not ids:
-        position_list = range(960)
+    elif args.range:
+        start, end = map(int, args.range.split("-"))
+        ids.update(range(start, end + 1))
     else:
-        position_list = sorted(ids)
-
-    if any(i not in range(960) for i in position_list):
-        parser.error("Position IDs must be between 0 and 959.")
+        ids = set(range(960))
 
     with chess.engine.SimpleEngine.popen_uci(args.stockfish) as stockfish:
         name = stockfish.id["name"]
@@ -103,7 +93,7 @@ def main():
             options["Hash"] = args.hash
         stockfish.configure(options)
 
-        for n in position_list:
+        for n in ids:
             board = chess.Board.from_chess960_pos(n)
             info = stockfish.analyse(board, chess.engine.Limit(depth=args.depth))
 
